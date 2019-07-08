044a967941a7d2a0fb4a21625b015ce6b0be415d
dheepakg <sslvgdek@gmail.com>
2019-07-08 11:21:40 +0530

    init

---
+++ atom/packages.sh
@@ -0,0 +1,8 @@
+apm install linter
+apm install atom-beautify
+apm install highlight-selected
+apm install file-icons
+apm install minimap
+apm install wordcount
+apm install block-select
+apm install wordcount
