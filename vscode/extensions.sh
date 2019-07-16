1121b1bd37ff10bc0b5b2126e5c3e8caf31635e2
dheepakg <mail2gdek@gmail.com>
2019-07-16 22:47:26 +0530

    added vscode

---
+++ vscode/extensions.sh
@@ -0,0 +1,12 @@
+code --install-extension esbenp.prettier-vscode
+code --install-extension eamodio.gitlens
+
+#Python Extensions
+code --install-extension zhuangtongfa.Material-theme
+code --install-extension ms-python.python
+code --install-extension njpwerner.autodocstring
+code --install-extension formulahendry.code-runner
+code --install-extension aaron-bond.better-comments
+
+#Rust Extensions
+code --install-extension rust-lang.rust
