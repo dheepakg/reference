9179b52f790514f65a230202587b1586bc700fe1
dheepakg <sslvgdek@gmail.com>
2020-05-01 18:09:48 +0530

    removed bad filed

--- atom/packages.sh
+++
@@ -1,8 +0,0 @@
-apm install linter
-apm install atom-beautify
-apm install highlight-selected
-apm install file-icons
-apm install minimap
-apm install wordcount
-apm install block-select
-apm install wordcount
--- rust-addons/rust-addons.sh
+++
@@ -1,9 +0,0 @@
-# Refer https://hoverbear.org/2017/03/03/setting-up-a-rust-devenv/
-#To install clippy - linter
-rustup component add clippy-preview
-
-#Code formatting
-cargo install rustfmt
-
-#If linker error appears (only for mac)
-sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
